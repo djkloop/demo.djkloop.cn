33e19b2cfc1eb7f81b66c63a957ab96e9ed988e2
djkloop <1044032256@qq.com>
2018-04-21 16:35:11 +0800

    feat(all): components/App.vue

---
+++ components/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="VueSSR">
+   当前页面由NuxtJS渲染.
+  </div>
+</template>
+
+<style>
+  .VueSSR {
+    font-size: 36px;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,20 @@
 <template>
   <section class="container">
     <div>
-      <app-logo/>
+      <app/>
       <h1 class="title">
         demo.djkloop.cn
       </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import App from '~/components/App.vue'
 
 export default {
   components: {
-    AppLogo
+    App
   }
 }
 </script>
@@ -50,15 +37,4 @@ export default {
   letter-spacing: 1px;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
